9cc16340b0a50c6c1bf17e9c13f233eeb0524157
Roy Mosby <ego.madking@gmail.com>
2025-02-06 18:59:24 -0500

    add newTodo state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm"
 
 function App() {
 
+  const [newTodo, setNewTodo ] = useState('Example Text');
+
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm/>
+      <p>{newTodo}</p>
       <TodoList/>
     </div>
   );
