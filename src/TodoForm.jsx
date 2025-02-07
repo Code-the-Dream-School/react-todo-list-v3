9821c9d814287cf6c996f9446fab3721da50824f
Roy Mosby <ego.madking@gmail.com>
2025-02-07 12:28:50 -0500

    add new todo to state

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,10 +1,19 @@
-function TodoForm(){
+function TodoForm({onAddTodo}){
+
+  function handleAddTodo(event){
+    event.preventDefault();
+    const title = event.target.title.value;
+    event.target.title.value = "";
+    const newTodo = {title, id: Date.now()};
+    onAddTodo(newTodo);
+  }
   return(
-    <form><label htmlFor="todoTitle">Todo</label>
-    <input type="text" id="todoTitle" />
-    <button>Add Todo</button>
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="todoTitle">Todo</label>
+      <input type="text" id="todoTitle" name="title" />
+      <button>Add Todo</button>
     </form>
   )
 }
 
-export default TodoForm
+export default TodoForm;
