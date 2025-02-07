d75013cd59d6e081c207716df9edbf9c157ae36b
Roy Mosby <ego.madking@gmail.com>
2025-02-06 19:00:35 -0500

    refactor list item into TodoListItem

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,18 @@
+import TodoListItem from "./TodoListItem"
+
+const todos = [
+  {id: 1, title: "review resources"},
+  {id: 2, title: "take notes"},
+  {id: 3, title: "code out app"},
+]
+
 function TodoList(){
 
-  const todos = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-  ]
+
     
   return(
     <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
       </ul>
   )
 }
