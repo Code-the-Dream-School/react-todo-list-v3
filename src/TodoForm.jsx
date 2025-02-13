ee037a9ad772e067f04403780c115bb29d35fe2f
Roy Mosby <ego.madking@gmail.com>
2025-02-13 11:42:32 -0500

    useRef to re-focus input (1.4)

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,15 +1,19 @@
+import { useRef } from 'react';
+
 function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef('');
   function handleAddTodo(event) {
     event.preventDefault();
     const title = event.target.title.value;
     event.target.title.value = '';
     const newTodo = { title, id: Date.now() };
     onAddTodo(newTodo);
+    todoTitleInput.current.focus();
   }
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" name="title" />
+      <input type="text" id="todoTitle" name="title" ref={todoTitleInput} />
       <button>Add Todo</button>
     </form>
   );
