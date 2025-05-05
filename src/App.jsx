05d4420796d1178ab7ee7cea6601fd3213061165
royemosby <roy.e.mosby@gmail.com>
2025-05-05 15:23:58 -0400

    remove unneeded todo update

--- src/App.jsx
+++ src/App.jsx
@@ -128,21 +128,6 @@ function App() {
       if (!resp.ok) {
         throw new Error(resp.error);
       }
-      const { records } = await resp.json();
-      const updatedTodo = {
-        id: records[0]['id'],
-        ...records[0].fields,
-      };
-      if (!records[0].fields.isCompleted) {
-        updatedTodo.isCompleted = false;
-      }
-      const updatedTodos = todoList.map((todo) => {
-        if (todo.id === updatedTodo.id) {
-          return { ...updatedTodo };
-        }
-        return todo;
-      });
-      setTodoList([...updatedTodos]);
     } catch (error) {
       console.dir(error);
       setErrorMessage(`${error.message}. Reverting todo...`);
@@ -183,27 +168,11 @@ function App() {
       },
       body: JSON.stringify(payload),
     };
-
     try {
       const resp = await fetch(url, options);
       if (!resp.ok) {
         throw new Error(resp.error);
       }
-      const { records } = await resp.json();
-      const updatedTodo = {
-        id: records[0]['id'],
-        ...records[0].fields,
-      };
-      if (!records[0].fields.isCompleted) {
-        updatedTodo.isCompleted = false;
-      }
-      const updatedTodos = todoList.map((todo) => {
-        if (todo.id === updatedTodo.id) {
-          return { ...updatedTodo };
-        }
-        return todo;
-      });
-      setTodoList([...updatedTodos]);
     } catch (error) {
       console.dir(error);
       setErrorMessage(`${error.message}. Reverting todo...`);
